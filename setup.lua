e3a24206989d9d1c18cba94eaffb56c06d98ac1c
Greg Johnson <gregj@alleninstitute.org>
2017-04-10 16:22:35 -0700

    added setting-dependent-on-options here

--- setup.lua
+++ setup.lua
@@ -1,69 +1,189 @@
 setup = {}
 
 
-function setup.getModelOpts()
+-- function setup.getModelOpts()
 
-    local model_opts = {}
-    model_opts.cuda = cuda
+--     local model_opts = {}
+--     opts.cuda = cuda
     
-    model_opts.parent_dir = 'AAE_shape_learner_v2'
-    model_opts.model_name = 'cell_learner_3_caae_learner'
-    model_opts.save_dir = model_opts.parent_dir .. '/' .. model_opts.model_name
-    model_opts.image_dir = '/root/images/2016_11_08_Nuc_Cell_Seg_8_cell_lines_V22/processed_aligned/2D'
-    model_opts.image_sub_size = 16.96875/2
+--     opts.parent_dir = 'AAE_shape_learner_v2'
+--     opts.modelName = 'cell_learner_3_caae_learner'
+--     opts.saveDir = opts.parent_dir .. '/' .. opts.modelName
+--     opts.image_dir = '/root/images/2016_11_08_Nuc_Cell_Seg_8_cell_lines_V22/processed_aligned/2D'
+--     opts.image_sub_size = 16.96875/2
 
-    model_opts.nLatentDims = 16
-    model_opts.channel_inds_in = torch.LongTensor{1,3}
-    model_opts.channel_inds_out = torch.LongTensor{1,3}
+--     opts.nLatentDims = 16
+--     opts.channel_inds_in = torch.LongTensor{1,3}
+--     opts.channel_inds_out = torch.LongTensor{1,3}
 
-    model_opts.rotate = true
-    model_opts.nChIn = model_opts.channel_inds_in:size(1)
-    model_opts.nChOut = model_opts.channel_inds_out:size(1)
-    model_opts.nClasses = 0
-    model_opts.nOther = 0
-    model_opts.dropoutRate = 0.2
-    model_opts.fullConv = true
+--     opts.rotate = true
+--     opts.nChIn = opts.channel_inds_in:size(1)
+--     opts.nChOut = opts.channel_inds_out:size(1)
+--     opts.nClasses = 0
+--     opts.nOther = 0
+--     opts.dropoutRate = 0.2
+--     opts.fullConv = true
     
-    model_opts.adversarialGen = false
+--     opts.adversarialGen = false
     
-    model_opts.test_model = false
-    model_opts.verbose = true
+--     opts.test_model = false
+--     opts.verbose = true
 
-    paths.mkdir(model_opts.parent_dir)
+--     paths.mkdir(opts.parent_dir)
 
-    return model_opts
+--     return model_opts
+-- end
+
+function setup.init(opts)
+    
+    opts.save = {}
+    opts.save.opts = opts.saveDir .. '/opts.t7'
+
+    opts.save.enc = opts.saveDir .. '/enc.t7'
+    opts.save.dec = opts.saveDir .. '/dec.t7'
+    opts.save.encD = opts.saveDir .. '/encD.t7'
+    opts.save.decD = opts.saveDir .. '/decD.t7'
+
+    opts.save.optEnc = opts.saveDir .. '/optEnc.t7'
+    opts.save.optDec = opts.saveDir .. '/optDec.t7'
+    opts.save.optEncD = opts.saveDir .. '/optEncD.t7'
+    opts.save.optDecD = opts.saveDir .. '/optDecD.t7'
+
+    opts.save.stateEnc = opts.saveDir .. '/stateEnc.t7'
+    opts.save.stateDec = opts.saveDir .. '/stateDec.t7'
+    opts.save.stateEncD = opts.saveDir .. '/stateEncD.t7'
+    opts.save.stateDecD = opts.saveDir .. '/stateDecD.t7'
+
+    opts.save.rng = opts.saveDir .. '/rng.t7'
+    opts.save.rngCuda = opts.saveDir .. '/rng_cuda.t7'
+
+    opts.save.tmpEmbeddings = opts.saveDir .. '/tmp_embeddings.t7'
+    opts.save.tmpPlots = opts.saveDir .. '/tmp_plots.t7'
+    opts.save.tmpEpoch = opts.saveDir .. '/tmp_epoch.t7'
+
+    opts.save.plots = opts.saveDir .. '/plots.t7'
+    opts.save.epoch = opts.saveDir .. '/epoch.t7'
+    
+    opts.save.data = opts.dataDir .. '/data_' .. opts.imsize .. '.t7'
+    
+    
+    if opts.skipGanD then
+        opts.useGanD = false
+    else
+        opts.useGanD = true
+    end
+    opts.image_sub_size = 1.4707*(512/opts.imsize)
+    
+        
+    opts.epoch = 0
+
+    optEnc = {}
+    optEnc.optimizer = opts.optimizer
+    optEnc.learningRate = opts.learningRate
+    optEnc.beta1 = opts.beta1  
+
+    optDec = {}
+    optDec.optimizer = opts.optimizer
+    optDec.learningRate = opts.learningRate
+    optDec.beta1 = opts.beta1
+
+    optAdv = {}
+    optAdv.optimizer = opts.optimizer
+    optAdv.learningRate = opts.learningRate
+    optAdv.beta1 = opts.beta1 
+
+    optAdvGen = {}
+    optAdvGen.optimizer = opts.optimizer
+    optAdvGen.learningRate = opts.learningRate
+    optAdvGen.beta1 = opts.beta1    
+
+    stateEnc = {}
+    stateDec = {}
+    stateAdv = {}
+    stateAdvGen = {}
+
+    losses, latentlosses, advlosses, advGenLosses, advMinimaxLoss, advGenMinimaxLoss, advlossesGen, reencodelosses = {}, {}, {}, {}, {}, {}, {}, {}, {}
+    
+    paths.mkdir(opts.dataDir)
+    
+    
+     if paths.filep(opts.save.opts) then
+        print('Loading previous optimizer state')
+
+        opts = torch.load(opts.save.opts)
+
+        optEnc = torch.load(opts.save.optEnc)
+        optDec = torch.load(opts.save.optDec)
+        optAdv = torch.load(opts.save.optEncD)
+        optAdvGen = torch.load(opts.save.optDecD)        
+
+        
+        stateEnc = utils.table2cuda(torch.load(opts.save.stateEnc))
+        stateDec = utils.table2cuda(torch.load(opts.save.stateDec))    
+        stateAdv = utils.table2cuda(torch.load(opts.save.stateEncD))
+        
+        
+        plots = torch.load(opts.save.plots)
+        
+        if paths.filep(opts.save.stateDecD) then
+            stateAdvGen = utils.table2cuda(torch.load(opts.save.stateDecD))
+            
+            losses =            plots[1][3]:totable()
+            latentlosses =      plots[2][3]:totable()
+            advlosses =         plots[3][3]:totable()
+            advGenLosses =      plots[4][3]:totable()
+            advMinimaxLoss =    plots[5][3]:totable()
+            advGenMinimaxLoss = plots[6][3]:totable()
+            reencodelosses =    plots[7][3]:totable()
+            
+        else
+            print('Could not find stateAdvGen_path: ' .. opts.save.stateDecD)
+            stateAdvGen = {}
+            
+            losses = plots[1][3]:totable()
+            latentlosses = plots[2][3]:totable()
+            advlosses = plots[3][3]:totable()
+            advMinimaxLoss = plots[4][3]:totable()
+            reencodelosses = plots[5][3]:totable()
+            
+
+        end
+    end
+
+    plots = {}
+    loss, advloss, advlossGen = {}, {}, {}
+    
+    return opts
 end
 
+function setup.getModel(opts)
 
+    print('Loading model type: ' .. opts.modelName)
 
-function setup.getModel()
-
-    print('Loading model type: ' .. model_opts.model_name)
-
-    package.loaded['models/' .. model_opts.model_name] = nil
-    Model = require ('models/' .. model_opts.model_name)
+    package.loaded['models/' .. opts.modelName] = nil
+    Model = require ('models/' .. opts.modelName)
     autoencoder = nil
     encoder = nil
     decoder = nil
 
-    if paths.filep(model_opts.save_dir .. '/decoder.t7') then
-        print('Loading model from ' .. model_opts.save_dir)
+    if paths.filep(opts.save.dec) then
+        print('Loading model from ' .. opts.saveDir)
         
         print('Loading encoder')
-        encoder = torch.load(model_opts.save_dir .. '/encoder.t7')
+        encoder = torch.load(opts.save.enc)
         encoder:float()
         encoder:clearState()
         collectgarbage()
         
         print('Loading adversary')
-        adversary = torch.load(model_opts.save_dir .. '/adversary.t7')
+        adversary = torch.load(opts.save.encD)
         adversary:float()
         adversary:clearState()
         collectgarbage()
         
-        if paths.filep(model_opts.save_dir .. '/adversaryGen.t7') then
+        if paths.filep(opts.save.decD) then
             print('Loading adversaryGen')
-            adversaryGen = torch.load(model_opts.save_dir .. '/adversaryGen.t7')
+            adversaryGen = torch.load(opts.save.decD)
             adversaryGen:float()
             adversaryGen:clearState()
             collectgarbage()
@@ -72,27 +192,27 @@ function setup.getModel()
         end
         
         print('Loading decoder')
-        decoder = torch.load(model_opts.save_dir .. '/decoder.t7')    
+        decoder = torch.load(opts.save.dec)    
         decoder:float()
         decoder:clearState()
         collectgarbage()
         
         
         
-        if paths.filep(model_opts.save_dir .. '/rng.t7') then
+        if paths.filep(opts.save.rng) then
             print('Loading RNG')
-            torch.setRNGState(torch.load(model_opts.save_dir .. '/rng.t7'))
-            cutorch.setRNGState(torch.load(model_opts.save_dir .. '/rng_cuda.t7'))
+            torch.setRNGState(torch.load(opts.save.rng))
+            cutorch.setRNGState(torch.load(opts.save.rngCuda))
         end
             
         
          -- print(gpuIDs)
-        set_gpu_id = function(model, gpuIDs)
+        setGpuID = function(model, gpuIDs)
             for i = 1, #model.modules do
                 model.modules[i].device = gpuIDs[i]
                 
                 if i == #model.modules then
-                    model.modules[i].outdevice = gpu1
+                    model.modules[i].outdevice = opts.gpu1
                 else
                     model.modules[i].outdevice = gpuIDs[i+1]
                 end
@@ -100,27 +220,28 @@ function setup.getModel()
             return model
         end
 
-        gpuIDs = torch.zeros(#encoder.modules):fill(gpu1)
+        gpuIDs = torch.zeros(#encoder.modules):fill(opts.gpu1)
         -- gpuIDs:sub(9,#encoder.modules):fill(gpu2)
-        encoder = set_gpu_id(encoder, gpuIDs)
+        encoder = setGpuID(encoder, gpuIDs)
 
-        gpuIDs = torch.zeros(#decoder.modules):fill(gpu2)
-        decoder = set_gpu_id(decoder, gpuIDs)
+        gpuIDs = torch.zeros(#decoder.modules):fill(opts.gpu2)
+        decoder = setGpuID(decoder, gpuIDs)
 
-        gpuIDs = torch.zeros(#adversary.modules):fill(gpu2)
-        adversary = set_gpu_id(adversary, gpuIDs)
+        gpuIDs = torch.zeros(#adversary.modules):fill(opts.gpu2)
+        adversary = setGpuID(adversary, gpuIDs)
 
         if adversaryGen ~= nil then
-            gpuIDs = torch.zeros(#adversaryGen.modules):fill(gpu3)
-            adversaryGen = set_gpu_id(adversaryGen, gpuIDs)
+            gpuIDs = torch.zeros(#adversaryGen.modules):fill(opts.gpu3)
+            adversaryGen = setGpuID(adversaryGen, gpuIDs)
         end
         
         print('Done loading model')
     else
         print('Creating new model')
-        print(model_opts)
-        Model:create(model_opts)
-        paths.mkdir(model_opts.save_dir)
+        
+        print(opts)
+        Model:create(opts)
+        paths.mkdir(opts.saveDir)
         print('Done creating model')
         
         decoder = Model.decoder
@@ -130,12 +251,12 @@ function setup.getModel()
         
 
         -- print(gpuIDs)
-        set_gpu_id = function(model, gpuIDs)
+        setGpuID = function(model, gpuIDs)
             for i = 1, #model.modules do
                 model.modules[i] = nn.GPU(model.modules[i], gpuIDs[i])
                 -- model.modules[i].device = gpuIDs[i]
                 if i == #model.modules then
-                    model.modules[i].outdevice = gpu1
+                    model.modules[i].outdevice = opts.gpu1
                 else
                     model.modules[i].outdevice = gpuIDs[i+1]
                 end
@@ -143,20 +264,21 @@ function setup.getModel()
             return model
         end
 
-        gpuIDs = torch.zeros(#encoder.modules):fill(gpu1)
+        gpuIDs = torch.zeros(#encoder.modules):fill(opts.gpu1)
         -- gpuIDs:sub(4,#encoder.modules):fill(gpu2)
-        encoder = set_gpu_id(encoder, gpuIDs)
+        encoder = setGpuID(encoder, gpuIDs)
 
-        gpuIDs = torch.zeros(#decoder.modules):fill(gpu2)
-        decoder = set_gpu_id(decoder, gpuIDs)
+        gpuIDs = torch.zeros(#decoder.modules):fill(opts.gpu2)
+        decoder = setGpuID(decoder, gpuIDs)
 
-        gpuIDs = torch.zeros(#adversary.modules):fill(gpu2)
-        adversary = set_gpu_id(adversary, gpuIDs)
+        gpuIDs = torch.zeros(#adversary.modules):fill(opts.gpu2)
+        adversary = setGpuID(adversary, gpuIDs)
 
-        gpuIDs = torch.zeros(#adversaryGen.modules):fill(gpu3)
-        adversaryGen = set_gpu_id(adversaryGen, gpuIDs)
+        gpuIDs = torch.zeros(#adversaryGen.modules):fill(opts.gpu3)
+        adversaryGen = setGpuID(adversaryGen, gpuIDs)
 
         opts_optnet = {inplace=true, mode='training'}
+        
         optnet.optimizeMemory(encoder, dataProvider:getImages(torch.LongTensor{1,2}, 'train'), opts_optnet)
         optnet.optimizeMemory(decoder, encoder.output, opts_optnet)
         optnet.optimizeMemory(adversary, encoder.output[#encoder.output], opts_optnet)
@@ -195,7 +317,7 @@ function setup.getModel()
         print('Done converting to cuda')
     end
     
-    if model_opts.test_model then
+    if opts.test_model then
 
         print('Data size')
         print(dataProvider.train.inds:size())
@@ -241,8 +363,8 @@ function setup.getModel()
         adversaryGen:training()
     end    
     
-    cudnn.benchmark = false
-    cudnn.fastest = false
+    cudnn.benchmark = opts.cudnnBenchmark or false
+    cudnn.fastest = opts.cudnnFastest or false
     
     cudnn.convert(encoder, cudnn)
     cudnn.convert(decoder, cudnn)
@@ -255,136 +377,6 @@ function setup.getModel()
     print('Done getting parameters')
 end
 
-function setup.getLearnOpts(model_opts)
-
-    opt_path = model_opts.save_dir .. '/opt.t7'
-    optEnc_path = model_opts.save_dir .. '/optEnc.t7'
-    optDec_path = model_opts.save_dir .. '/optDec.t7'
-    optAdv_path = model_opts.save_dir .. '/optD.t7'
-    optAdvGen_path = model_opts.save_dir .. '/optAdvGen.t7'
-    
-    stateEnc_path = model_opts.save_dir .. '/stateEnc.t7'
-    stateDec_path = model_opts.save_dir .. '/stateDec.t7'
-    stateAdv_path = model_opts.save_dir .. '/stateD.t7'
-    stateAdvGen_path = model_opts.save_dir .. '/stateAdvGen.t7'
-
-    plots_path = model_opts.save_dir .. '/plots.t7'
-    
-    if paths.filep(opt_path) then
-        print('Loading previous optimizer state')
-
-        opt = torch.load(opt_path)
-
-        optEnc = torch.load(optEnc_path)
-        optDec = torch.load(optDec_path)
-        optAdv = torch.load(optAdv_path)
-        optAdvGen = torch.load(optAdv_path)        
-
-        
-        stateEnc = utils.table2cuda(torch.load(stateEnc_path))
-        stateDec = utils.table2cuda(torch.load(stateDec_path))    
-        stateAdv = utils.table2cuda(torch.load(stateAdv_path))
-        
-        
-        plots = torch.load(plots_path)
-        
-        if paths.filep(stateAdvGen_path) then
-            stateAdvGen = utils.table2cuda(torch.load(stateAdvGen_path))
-            
-            losses = plots[1][3]:totable()
-            latentlosses = plots[2][3]:totable()
-            advlosses = plots[3][3]:totable()
-            advGenLosses = plots[4][3]:totable()
-            advMinimaxLoss = plots[5][3]:totable()
-            advGenMinimaxLoss = plots[6][3]:totable()
-            reencodelosses = plots[7][3]:totable()
-            
-        else
-            print('Could not find stateAdvGen_path: ' .. stateAdvGen_path)
-            stateAdvGen = {}
-            
-            losses = plots[1][3]:totable()
-            latentlosses = plots[2][3]:totable()
-            advlosses = plots[3][3]:totable()
-            advMinimaxLoss = plots[4][3]:totable()
-            reencodelosses = plots[5][3]:totable()
-        end
-
-    else
-        opt = {}
-
-        opt.epoch = 0
-        opt.nepochs = 2000
-        opt.adversarial = true
-        opt.adversarialGen = model_opts.adversarialGen
-
-        opt.learningRateA = 0.01
-        opt.learningRateAdv = 0.01
-
-        opt.min_rateA =  0.0000001
-        opt.min_rateD =  0.0001
-        opt.learningRateDecay = 0.999
-        opt.optimizer = 'adam'
-        opt.batchSize = 64
-        opt.verbose = model_opts.verbose
---         opt.updateD = 0.50
---         opt.updateA = 0.40
-        opt.update_thresh = 0.58
-        opt.saveProgressIter = 5
-        opt.saveStateIter = 50
-
-
-        optEnc = {}
-        optEnc.optimizer = opt.optimizer
-        optEnc.learningRate = opt.learningRateA
-        optEnc.min_rateA =  0.0000001
-        optEnc.beta1 = 0.5        
-        -- optEnc.momentum = 0
-        -- optEnc.numUpdates = 0
-        -- optEnc.coefL2 = 0
-        -- optEnc.coefL1 = 0
-
-        optDec = {}
-        optDec.optimizer = opt.optimizer
-        optDec.learningRate = opt.learningRateA
-        optDec.min_rateA =  0.0000001
-        optDec.beta1 = 0.5
-        -- optDec.momentum = 0
-        -- optDec.numUpdates = 0
-        -- optDec.coefL2 = 0
-        -- optDec.coefL1 = 0
-        
-        optAdv = {}
-        optAdv.optimizer = opt.optimizer
-        optAdv.learningRate = opt.learningRateAdv
-        optAdv.min_rateA =  0.0000001
-        optAdv.beta1 = 0.5        
-        -- optAdv.momentum = 0
-        -- optAdv.numUpdates = 0
-        -- optAdv.coefL2 = 0
-        -- optAdv.coefL1 = 0
-        
-        optAdvGen = {}
-        optAdvGen.optimizer = opt.optimizer
-        optAdvGen.learningRate = opt.learningRateAdv
-        optAdvGen.min_rateA =  0.0000001
-        optAdvGen.beta1 = 0.5        
-        -- optAdvGen.momentum = 0
-        -- optAdvGen.numUpdates = 0
-        -- optAdvGen.coefL2 = 0
-        -- optAdvGen.coefL1 = 0
-
-        stateEnc = {}
-        stateDec = {}
-        stateAdv = {}
-        stateAdvGen = {}
-        
-        losses, latentlosses, advlosses, advGenLosses, advMinimaxLoss, advGenMinimaxLoss, advlossesGen, reencodelosses = {}, {}, {}, {}, {}, {}, {}, {}, {}
-    end
-
-    plots = {}
-    loss, advloss, advlossGen = {}, {}, {}
-end
 
 return setup
 
